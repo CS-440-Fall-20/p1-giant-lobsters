775058721bc2622bbfb2f0c8d4a4feab403927b7
Behjet <31242852+Behjet@users.noreply.github.com>
2020-11-15 21:21:34 +0500

    Views, flat and smooth shading added.

--- simulator.js
+++ simulator.js
@@ -2,15 +2,29 @@
 
 var canvas;
 var gl;
+var program;
 
 // Arrays to populate buffers
 var points = [];
 var normals = [];
 var colours = []; 
+var faces = {}; // dictionary for storing normals for smooth shading
 
 // GLobal buffers
 var vertexBuffer;
 var colourBuffer;
+var normalBuffer;
+
+//for flat shading
+var lightPosition = vec4(1.0, 0.5, 1.0, 0.0 );
+var lightAmbient = vec4(0.8, 0.8, 0.8, 1.0 );
+var lightDiffuse = vec4( 1.0, 1.0, 1.0, 1.0 );
+var lightSpecular = vec4( 1.0, 1.0, 1.0, 1.0 );
+var materialAmbient = vec4( 1.0, 1.0, 1.0, 1.0 );
+var materialDiffuse = vec4( 1.0, 0.8, 0.0, 1.0 );
+var materialSpecular = vec4( 1.0, 1.0, 1.0, 1.0 );
+var materialShininess = 30.0;
+
 
 //Viewing volume parameters
 var near = -1;
@@ -28,10 +42,16 @@ var eye;
 var atVector = vec3(0.0, 3.0, 0.0);
 var upVector = vec3(0.0, 1.0, 0.0);
 
-// views
+//Shading
+var flatShading = false;
+var smoothShading = false;
+
+// Views
 var wireframe = true;
 var pointsView = false;
+var faceView = flatShading;
 
+// Matrices
 var modelViewMatrix, modelViewMatrixLoc;
 var projectionMatrix, projectionMatrixLoc;
 var normalMatrix, normalMatrixLoc;
@@ -44,9 +64,7 @@ var minAltitude = 2.8; // min altitude
 var maxAltitude = 3.5; // max altitude
 var S = 0.1; // max speed
 
-var color = vec4(1, 1, 1, 1); //colour of wireframe terrain (white)
 var scale = 0.1; //arbitary scaling factor for wireframe
-
 
 
 window.onload = function init() {
@@ -64,35 +82,50 @@ window.onload = function init() {
     //
     //  Load shaders and initialize attribute buffers
     //
-    var program = initShaders(gl, "vertex-shader", "fragment-shader");
+    program = initShaders(gl, "vertex-shader", "fragment-shader");
     gl.useProgram(program);
-
-
     
     getPatch(-10,10,-10,10);
+
+    initBuffers();
+}
+
+function initBuffers(){
+
+    var ambientProd = mult(lightAmbient, materialAmbient);
+	var diffuseProd = mult(lightDiffuse, materialDiffuse);
+	var specularProd = mult(lightSpecular, materialSpecular);
+    
+    normalBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(normals), gl.STATIC_DRAW);
+    var vNormal = gl.getAttribLocation(program, "vNormal");
+    gl.vertexAttribPointer(vNormal, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vNormal);
 
     vertexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
-
     var vPosition = gl.getAttribLocation(program, "vPosition");
     gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
-
-    modelViewMatrixLoc = gl.getUniformLocation(program, "modelViewMatrix");
-    projectionMatrixLoc = gl.getUniformLocation(program, "projectionMatrix");
-    // normalMatrixLoc = gl.getUniformLocation(program, "normalMatrix");
-
 
     // Load colours data into the GPU
     colourBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, colourBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(colours), gl.STATIC_DRAW);
-
     var vColour = gl.getAttribLocation(program, "vColour");
     gl.vertexAttribPointer(vColour, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vColour);
 
+    modelViewMatrixLoc = gl.getUniformLocation(program, "modelViewMatrix");
+    projectionMatrixLoc = gl.getUniformLocation(program, "projectionMatrix");
+    normalMatrixLoc = gl.getUniformLocation(program, "normalMatrix");
+    gl.uniform4fv( gl.getUniformLocation(program, "ambientProd"),flatten(ambientProd) );
+	gl.uniform4fv( gl.getUniformLocation(program,"diffuseProd"),flatten(diffuseProd) );
+	gl.uniform4fv( gl.getUniformLocation(program,"specularProd"),flatten(specularProd) );
+	gl.uniform4fv( gl.getUniformLocation(program,"lightPosition"),flatten(lightPosition) );
+	gl.uniform1f( gl.getUniformLocation(program,"shininess"),materialShininess );
 
     document.body.onkeydown = function (event) {
         event = event || window.event;
@@ -118,31 +151,60 @@ function getPatch(xmin, xmax, zmin, zmax) {
 
     for (var i = xmin; i < xmax; i += scale) {
         for (var j = zmin; j < zmax; j += scale) {
-            //basic mesh  (triangulated terrain )
-            points.push(vec3(i, 0, j + scale));
-            points.push(vec3(i, 0, j));
-            points.push(vec3(i + scale, 0, j));
+            if (wireframe){
+                //basic mesh  (triangulated terrain )
+                points.push(vec3(i, 0, j + scale));
+                points.push(vec3(i, 0, j));
+                points.push(vec3(i + scale, 0, j));
 
-            points.push(vec3(i, 0, j + scale));
-            points.push(vec3(i + scale, 0, j + scale));
-            points.push(vec3(i, 0, j + scale));
-
-            colours.push(color);
-            colours.push(color);
-            colours.push(color);
-
-            colours.push(color);
-            colours.push(color);
-            colours.push(color);
-
+                points.push(vec3(i, 0, j + scale));
+                points.push(vec3(i + scale, 0, j + scale));
+                points.push(vec3(i, 0, j + scale));
+            }
+            else if(flatShading || smoothShading || faceView){
+                points.push(vec3(i, 0, j));
+				points.push(vec3(i, 0, j + scale));
+				points.push(vec3(i + scale, 0, j));
+				points.push(vec3(i + scale, 0, j + scale));
+				points.push(vec3(i + scale, 0, j));
+				points.push(vec3(i, 0, j + scale));	
+            }
         }
         //randomly perturb y-coordinates - using perlin noise for smoother peaks
         for (var k = 0; k < points.length; k++) {
             points[k][1] = noise.perlin2(points[k][0], points[k][2]);
         }
     }
+    helper();
 }
 
+function helper(){
+
+    //For both flatShading and smoothShading the normals are generated
+    if (flatShading || faceView){
+        for (var i=1; i < points.length; i++){
+            if (points[i] === undefined || points[i+1] === undefined){
+                break;
+            }
+            var v1 = subtract(points[i], points[i-1]);
+            var v2 = subtract(points[i+1], points[i-1]);
+            var norm = normalize(cross(v2, v1));
+            normals.push(vec4(norm));
+        }
+    }
+    else if (smoothShading){             
+        var val = vec4(0, 0, 0, 0);
+        var val1, val2, val3, val4;
+        
+        for (var key in faces){
+            val1 = add(val, faces[key])[0]/3;
+            val2 = add(val, faces[key])[1]/3;	
+            val3 = add(val, faces[key])[2]/3;			
+            val4 = add(val, faces[key])[3]/3;
+            normals.push(vec4(val1,val2,val3,val4));
+        }
+    }
+}
 
 
 function render() {
@@ -152,12 +214,36 @@ function render() {
     flyingOffset += speed;
     noise.seed(10);
 
-    for (var k = 0; k < points.length; k++) {
-        points[k][1] = noise.perlin2(points[k][0] + flyingOffset, points[k][2]);
+    colours = [];
+    if (smoothShading){
+        normals = [];
     }
+    for (var k = 0; k < points.length; k++) {
+        points[k][1] = noise.perlin2(points[k][0] - flyingOffset, points[k][2]);
+
+		if (flatShading || smoothShading || faceView || wireframe){
+            if (points[k][1] < -0.45){
+                colours.push(vec4(0,0,1.0,1.0));        //blue
+            }
+            else if (points[k][1] >= -0.5 && points[k][1] < -0.15){
+                colours.push(vec4(0.1,0.7,0,1.0));          ///green
+            }
+            else if( points[k][1] >= 0.4){
+                colours.push(vec4(1, 1, 1, 1));            //white
+            }
+            else{
+                colours.push(vec4(0.6,0.4,0.2,1));     ///brown
+            }
+        }	
+	}
+
 
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
+    gl.bindBuffer( gl.ARRAY_BUFFER, normalBuffer );
+	gl.bufferData( gl.ARRAY_BUFFER, flatten(normals), gl.STATIC_DRAW );
+	gl.bindBuffer( gl.ARRAY_BUFFER, colourBuffer );
+	gl.bufferData( gl.ARRAY_BUFFER, flatten(colours), gl.STATIC_DRAW );
     
 
     //Flyby View
@@ -165,10 +251,13 @@ function render() {
 
     modelViewMatrix = lookAt(eye, atVector, upVector);
     projectionMatrix = ortho(left, right, bottom, ytop, near, far); //orthographic view
+    normalMatrix = [vec3(modelViewMatrix[0][0], modelViewMatrix[0][1], modelViewMatrix[0][2]),
+        vec3(modelViewMatrix[1][0], modelViewMatrix[1][1], modelViewMatrix[1][2]),
+        vec3(modelViewMatrix[2][0], modelViewMatrix[2][1], modelViewMatrix[2][2])];
 
     gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
     gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix));
-    // gl.uniformMatrix3fv( normalMatrixLoc, false, flatten(normalMatrix) );
+    gl.uniformMatrix3fv( normalMatrixLoc, false, flatten(normalMatrix) );
 
     if(wireframe)
     {
@@ -178,8 +267,10 @@ function render() {
     {
         gl.drawArrays(gl.POINTS, 0, points.length);
     }
-
-    //TODO: Add Face view
+    else if(flatShading || smoothShading || faceView)
+    {
+        gl.drawArrays(gl.TRIANGLES, 0, points.length)
+    }
 
     window.requestAnimFrame(render);
 }
@@ -218,8 +309,10 @@ function varyView(keycode, shiftPressed) {
     {
         toggleViews();
     }
-    console.log(near, far);
-
+    else if(keycode === "KeyC")
+    {
+        toggleShading();
+    }
 }
 
 
@@ -303,18 +396,50 @@ function varyFar(mode) {
 }
 
 
-// TODO:Add Shaded View 
 function toggleViews()
 {
     if(wireframe)
     {
         wireframe = false;
-        pointsView = true;
+        normals = [];
+        pointsView = smoothShading = false;
+        flatShading = faceView = true;
     }
     else if(pointsView)
     {
         wireframe = true;
         pointsView = false;
+        flatShading = faceView = smoothShading = false;
+    }
+    else if(faceView || smoothShading || flatShading){
+        flatShading = faceView = smoothShading = false;
+        pointsView = true;
+        wireframe = false;
+    }
+}
+
+function toggleShading()
+{
+    if(wireframe){
+        flatShading = faceView = true;
+        smoothShading = wireframe = pointsView = false;
+        normals = [];
+        getPatch(-10,10,-10,10);
+    }
+    else if(flatShading || faceView){
+        smoothShading = true;
+        flatShading = wireframe = faceView = pointsView = false;
+        normals = [];
+        colours = [];
+        points = [];
+        getPatch(-10,10,-10,10);
+        initBuffers();
+    }
+    else if(smoothShading){         //toggle to phong shading here
+        wireframe = true;
+        smoothShading = flatShading = faceView = pointsView = false;
+        normals = [];
+        getPatch(-10,10,-10,10);
     }
 }
 
